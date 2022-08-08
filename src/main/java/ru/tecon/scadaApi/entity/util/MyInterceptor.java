bf81b1a7fafbaef8af6a14f607de6e850b01d91f
Maksim <shchelkonogov@mail.ru>
2022-08-08 13:00:58 +0300

    Добавил перехватчик входных данных

---
+++ src/main/java/ru/tecon/scadaApi/entity/util/MyInterceptor.java
@@ -0,0 +1,31 @@
+package ru.tecon.scadaApi.entity.util;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.ext.ReaderInterceptorContext;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * Перехватчик сообщений переданных по JAX-RS
+ * @author Maksim Shchelkonogov
+ */
+@Provider
+public class MyInterceptor implements ReaderInterceptor {
+
+    private static final Logger LOGGER = Logger.getLogger(MyInterceptor.class.getName());
+
+    @Override
+    public Object aroundReadFrom(ReaderInterceptorContext context) throws IOException, WebApplicationException {
+        InputStream in = context.getInputStream();
+        String body = new BufferedReader(new InputStreamReader(in))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        LOGGER.log(Level.INFO, "incoming data {0}", body);
+        context.setInputStream(new ByteArrayInputStream(body.getBytes()));
+        return context.proceed();
+    }
+}
